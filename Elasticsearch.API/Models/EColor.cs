744196043b1b55408f3a904481b06db63f9c967f
Faru Nuri Sönmez <info@farunurisonmez.com>
2024-09-25 09:53:14 +0300

    Fix issue #4: Product and ProductFeature models with one-to-one relationship for Elasticsearch indexing

---
+++ Elasticsearch.API/Models/EColor.cs
@@ -0,0 +1,9 @@
+﻿namespace Elasticsearch.API.Models
+{
+    public enum EColor
+    {
+        Red = 0,
+        Green = 1,
+        Blue = 2,
+    }
+}
---
+++ Elasticsearch.API/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using Nest;
+
+namespace Elasticsearch.API.Models
+{
+    public class Product
+    {
+        [PropertyName("_id")]
+        public int Id { get; set; } 
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+
+        public ProductFeature? Feature { get; set; }
+    }
+}
