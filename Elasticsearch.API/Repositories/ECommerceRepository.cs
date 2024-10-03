bc7dd6675d36a40806aa1ca19898f99a6868c8a3
Faru Nuri Sönmez <info@farunurisonmez.com>
2024-10-03 15:03:52 +0300

    Fix issue #9: Implement Term Query for customer first name using elastic.clients.elasticsearch

---
+++ Elasticsearch.API/Repositories/ECommerceRepository.cs
@@ -0,0 +1,27 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.Eql;
+using Elasticsearch.API.Models.ECommerceModel;
+using System.Collections.Immutable;
+
+namespace Elasticsearch.API.Repositories
+{
+    public class ECommerceRepository
+    {
+        private readonly ElasticsearchClient _client;
+        private const string indexName = "kibana_sample_data_ecommerce";
+
+        public ECommerceRepository(ElasticsearchClient client) { 
+            _client = client;
+        }
+
+        public async Task<ImmutableList<ECommerce>> TermQuery(string customerFirstName)
+        {
+            var result = await _client.SearchAsync<ECommerce>(s => s.Index(indexName).Query(q => q.Term(t => t.Field
+            ("customer_first_name.keyword").Value(customerFirstName))));
+
+            foreach (var hit in result.Hits) hit.Source.Id = hit.Id;
+
+            return result.Documents.ToImmutableList();
+        }
+    }
+}
